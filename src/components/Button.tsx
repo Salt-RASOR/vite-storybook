0266a3ce58c0a1f07ddc1f5495f88c4fe82215f1
Svitlana <rsv0011@gmail.com>
2023-12-14 10:09:29 +0100

    feat: add the loader

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,20 @@
-import React from "react";
 import clsx from "clsx";
+import {PacmanLoader} from 'react-spinners'
+ 
+
 
 const Button = () => {
-
+const isLoading = true
   return (
     <button
       type="button"
       className={clsx("text-white bg-blue-700 font-medium rounded-lg text-sm", // button style
+      "flex ", 
       "px-5 py-2.5 me-2 mb-2", // layout
       "hover:bg-blue-800", // hover
       "focus:ring-4 focus:ring-blue-300   focus:outline-none")}>
-      Confirm
+        {isLoading && <p className="mr-8"><PacmanLoader size={8} color={"white"}/></p>}
+      <p>Confirm</p>
     </button>
   );
 };
